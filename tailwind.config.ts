b84aeef674fcdef23b1a8661e939273f31cd2c48
ArkcV <andrielmarques@hotmail.com>
2025-02-21 19:53:54 -0300

    feat : add config based styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,34 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: {
+          cf_white: "#FFFFFF",
+        },
+        black: {
+          cf: "#2B2B2B",
+        },
+        yellow: {
+          cf_yellow: "#DEBA3B",
+        },
+        blue: {
+          cf_blue: "#1A1363",
+        },
+        purple: {
+          cf_purple1: "#77749B",
+          cf_purple2: "#332F64",
+        },
+        gray: {
+          cf_gray1: "#ECE9E9",
+          cf_gray2: "#969292",
+          cf_gray3: "#4B4B4B",
+        },
+      },
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
   },
   plugins: [],
 }
