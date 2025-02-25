ab4904ed75a9bf7c33e4ac295f4675f19793dabc
ArkcV <andrielmarques@hotmail.com>
2025-02-25 12:58:58 -0300

    feat : add authentication of user

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,34 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  pages: {
+    signIn: "/",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          return null;
+        }
+
+        if (credentials.email === "Garagemcross@gmail.com" && credentials.password === "123456Am") {
+          return {
+            id: "1",
+            name: "GaragemCross",
+            email: "Garagemcross@gmail.com",
+          };
+        }
+        return null;
+      },
+    }),
+  ],
+
+});
+
+export { handler as GET, handler as POST };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,39 @@
 'use client';
 
 import Image from 'next/image';
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+  const router = useRouter();
 
+  async function handleLogin(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+  
+    const formData = new FormData(e.currentTarget);
+    const data = {
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
+    };
+  
+    console.log("Dados do formulário:", data);
+
+    const result = await signIn("credentials", {
+      ...data,
+      redirect: false, 
+    });
+
+    if (result?.ok) {
+      router.push("/dashboard");
+    } else {
+      console.error("Erro ao fazer login", result?.error);
+    }
+  }
+  
   return (
     <div className="flex items-center justify-center min-h-screen bg-white-cf_white">
       <div className="relative flex flex-col m-2 space-y-8 bg-gray-cf_gray1 shadow-2xl rounded-2xl md:flex-row md:space-y-0">
-        <div className="flex flex-col justify-center p-8 md:p-14">
+        <form onSubmit={handleLogin} className="flex flex-col justify-center p-8 md:p-14">
           <span className="mb-3 text-4xl font-bold text-blue-cf_blue">Bem-Vindo</span>
           <span className="font-light text-gray-400 mb-8">
             Bem-vindo de volta!, Por favor insira seus dados.
@@ -15,10 +41,10 @@ export default function Login() {
           <div className="py-4">
             <span className="mb-2 text-md font-bold text-blue-cf_blue">Email</span>
             <input
-              type="text"
+              type="email"
               name='email'
               className="w-full p-2 border-2 border-blue-cf_blue rounded-md placeholder:font-light placeholder:text-gray-500 focus:border-blue-cf_blue focus:outline-none"
-              placeholder="Digite sua Email"
+              placeholder="Digite seu Email"
               autoComplete='email'
             />
           </div>
@@ -33,20 +59,15 @@ export default function Login() {
           </div>
           <div className="flex justify-between w-full py-4">
             <label className="flex items-center">
-              <input
-                type="checkbox"
-                name='ch'
-                className="mr-2"
-              />
+              <input type="checkbox" name='ch' className="mr-2" />
               <span className="text-md">Lembrar-me</span>
             </label>
             <span className="text-base cursor-pointer text-blue-cf_blue">Recuperar Senha?</span>
           </div>
-          <button className="w-full bg-blue-cf_blue text-white-cf_white text-xl font-semibold p-2 rounded-xl mb-6 hover:bg-blue-950 
-             disabled:bg-blue-cf_blue">
+          <button className="w-full bg-blue-cf_blue text-white-cf_white text-xl font-semibold p-2 rounded-xl mb-6 hover:bg-blue-950">
             Entrar
           </button>
-        </div>
+        </form>
         <div className="relative hidden md:block">
           <Image
             src="/crossflex-logo.png"
