3bb1cf74c3c23eb498168952c21ad6f9d9aad6fd
ArkcV <andrielmarques@hotmail.com>
2025-04-01 15:41:21 -0300

    feat; add Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Image from "next/image"
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='flex px-10 mb-2'>
+      <Image
+          src="/crossflex-logo-page.png"
+          alt="crossflex-logo-page"
+          width={130}
+          height={50}
+          priority
+        />
     </div>
   )
 }
