512673a328e17029ba890baf45504dccb4274b4c
ArkcV <andrielmarques@hotmail.com>
2025-04-06 15:40:15 -0300

    feat: ad initial dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 const dashboard = () => {
   return (
-    <div>
-      dashboard 
+    <div className='flex gap-4 mt-4 overflow-auto scrollbar-none'>
+      <div className='main'>
+        <div className='cards'>
+          CARD 1
+        </div>
+        <div className='flex '>
+        <div className='transactions'>CARD 2</div>
+        <div className='Chat'>CARD 3</div>
+        </div>
+      </div>
+
+      <div className='side'>
+        <div className='Rigthbar'>
+          CARD 4
+        </div>
+        <div className='transactions'>CARD 5</div>
+      </div>
     </div>
   )
 }
